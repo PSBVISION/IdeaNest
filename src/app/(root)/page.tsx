aa37ddc9501210beb1c51a315084ba50fc490254
psbvision <punitsinghbisht1529@gmail.com>
2024-12-22 13:40:27 +0530

    data fetching and type generations done

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,24 +1,29 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/lib/queries";
+import { client } from "@/sanity/lib/client";
+
 export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: 'Badmassssh' },
-      _id: 1,
-      description: "This is a description",
-      image:
-        "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=808w=2940Sauto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  // const posts = [
+  //   {
+  //     _createdAt: new Date(),
+  //     views: 55,
+  //     author: { _id: 1, name: 'Badmassssh' },
+  //     _id: 1,
+  //     description: "This is a description",
+  //     image:
+  //       "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=808w=2940Sauto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     category: "Robots",
+  //     title: "We Robots",
+  //   },
+  // ];
+  const posts = await client.fetch(STARTUPS_QUERY);
+  console.log(JSON.stringify(posts, null, 2));
   return (
     <div>
       <section className="pink_container">
@@ -37,8 +42,17 @@ export default async function Home({
           {query ? `Search results for "${query}"` : "Trending Startup Ideas"}
         </p>
         <ul className="mt-7 card_grid">
+          {/* {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )} */}
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType) => <StartupCard key={post ?._id} post={post} />)
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
           ) : (
             <p className="no-results">No startups found</p>
           )}
