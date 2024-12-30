41f320635000c9cd6ff6b827c23693bcd4f7970d
psbvision <punitsinghbisht1529@gmail.com>
2024-12-30 14:20:11 +0530

    npm maaar gaya hai

--- src/app/(root)/startup/[id]/page.tsx
+++ src/app/(root)/startup/[id]/page.tsx
@@ -17,7 +17,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
       <section className="section_container">
-        <img src={post.image} alt="image" h />
+        <img src={post.image} alt="image" />
       </section>
     </>
   );
